4ce4d70e662448bdaf7377fc1b3a7d70eca2a36b
Night-Cheza <leila.kassi@outlook.com>
2022-05-24 19:40:33 -0600

    fixed responsiveness for small phones and tablets

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
+// $(document).ready(function() {
     
- /*Sticky navigation */    
-    $('.js--section-features').waypoint(function(direction){
-        if (direction =="down") {
-           $('nav').addClass('sticky');
-        } else {
-           $('nav').removeClass('sticky');    
-        }
-    },  {
-          offset:'55px;'
-        });
+//  /*Sticky navigation */    
+//     $('.js--section-features').waypoint(function(direction){
+//         if (direction =="down") {
+//            $('nav').addClass('sticky');
+//         } else {
+//            $('nav').removeClass('sticky');    
+//         }
+//     },  {
+//           offset:'55px;'
+//         });
     
 
-/*Scroll on buttons*/                             
-    $('.js--scroll-to-plans').click(function() {
-       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);   
-    });
+// /*Scroll on buttons*/                             
+//     $('.js--scroll-to-plans').click(function() {
+//        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);   
+//     });
     
-    $('.js--scroll-to-start').click(function() {
-       $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);   
-    });
+//     $('.js--scroll-to-start').click(function() {
+//        $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);   
+//     });
      
-});
+// });
